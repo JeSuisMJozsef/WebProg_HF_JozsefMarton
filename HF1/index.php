5f40db38600e3052ae6488fc3bd7674209e35c98
“JeSuisJocco” <martonjozsef@uni.sapientia.ro>
2021-09-21 18:24:55 +0300

    negyedik

--- HF1/index.php
+++ HF1/index.php
@@ -4,23 +4,6 @@ echo "Hello World!Vegso";
 ?>
 <br>
 <img src="./img/facepalm.gif" alt="alt"/>
-<table border="1">
-    <thead>
-        <tr>
-            <th>elso</th>
-            <th>masadik</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>elsoelso</td>
-            <td>masadikmasadik</td>
-        </tr>
-        <tr>
-            <td>harmadik</td>
-            <td>negyedik</td>
-        </tr>
-    </tbody>
-</table>
+
 
 
