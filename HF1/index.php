d1b0ae640014f26ba7ac13da7045f298b6e9e82a
“JeSuisJocco” <martonjozsef@uni.sapientia.ro>
2021-09-21 19:02:56 +0300

    shfile

--- HF1/index.php
+++ HF1/index.php
@@ -4,7 +4,7 @@ echo "Hello World!Vegso";
 ?>
 <br>
 <img src="./img/facepalm.gif" alt="alt"/>
-<h1>végső teszt push sh fileból</h1>
+<h1>végső teszt push sh fileból, sudo nelkul, keyboard shortcutbol</h1>
 
 
 
